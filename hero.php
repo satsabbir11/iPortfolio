37084ddb71d6f1402adbdfb46ce65788f9fcfeb1
SABBIR AHMED TALUKDAR <satsabbir11@gmail.com>
2021-06-06 16:08:29 +0600

    check

--- hero.php
+++ hero.php
@@ -1,8 +1,8 @@
 <!-- ======= Hero Section ======= -->
 <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
     <div class="hero-container" data-aos="fade-in">
-        <h1>Alex Smith</h1>
-        <p>I'm <span class="typed" data-typed-items="Designer, Developer, Freelancer, Photographer"></span></p>
+        <h1>Raiyan Rady</h1>
+        <p>I'm <span class="typed" data-typed-items="Front End Developer, Backend Developer, Wordpress Developer, Full Stack Developer"></span></p>
     </div>
 </section><!-- End Hero -->
 
